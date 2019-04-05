b95c6c29371fe8a8edfdc1218ddc42144a8f45cf
Abimola Oladele <31113550+consoledot@users.noreply.github.com>
2019-04-05 18:06:32 +0100

    add via upload
    DOM manipulations to Menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 const menu = document.querySelector('.menu');
 let topOfmenu = menu.offsetTop;
+const navigation = document.querySelector('nav');
+const menuIcon = document.querySelector('.menu-icon');
+const navigationList = [...document.querySelectorAll('ul li')];
 
 const stickNav = ()=>{
     if(window.scrollY >= topOfmenu){
         document.body.style.paddingTop = menu.offsetHeight + 'px';
         menu.classList.add('fixed-nav');
+        menu.style.height = '';
     }
     else{
         menu.classList.remove('fixed-nav');
         document.body.style.paddingTop =0;
+        menu.style.height= '70px';
     }
 }
 
 window.addEventListener('scroll', stickNav);
-
-const menuIcon = document.getElementsByClassName('menu-icon')[0];
-menuIcon.addEventListener('click', ()=>{
+const displayNav = ()=>{
     menuIcon.classList.toggle('change');
-});
+   if(navigation.style.display === '' || navigation.style.display === 'none'){
+         navigation.style.display = 'flex';
+    }else{
+       navigation.style.display='';  
+   }
+}
+
+navigationList.forEach(list=>{
+    list.addEventListener('click',displayNav);
+})
+menuIcon.addEventListener('click', displayNav);
+
+
+
